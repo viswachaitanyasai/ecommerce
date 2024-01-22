8c80fe0d7eb4c44c71919a81af0fa057183bae5a
Viswa Chaitanya Sai Jada <viswachaitanya444@gmail.com>
2024-01-23 02:51:01 +0530

    update prices

--- client/src/components/Prices.jsx
+++ client/src/components/Prices.jsx
@@ -1,23 +1,23 @@
 export const Prices = [
     {
         _id:0,
-        name:"Rs. 0 - 500",
-        array:[0,500]
+        name:"Rs. 0 - 499",
+        array:[0,499]
     },
     {
         _id:1,
-        name:"Rs. 500 - 1000",
-        array:[500,1000]
+        name:"Rs. 500 - 999",
+        array:[500,999]
     },
     {
         _id:2,
-        name:"Rs. 1000 - 5000",
-        array:[1000,5000]
+        name:"Rs. 1000 - 4999",
+        array:[1000,4999]
     },
     {
         _id:3,
-        name:"Rs. 5000 - 10,000",
-        array:[5000,10000]
+        name:"Rs. 5000 - 9999",
+        array:[5000,9999]
     },
     {
         _id:4,
