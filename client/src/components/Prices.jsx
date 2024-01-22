89c63a6b7539d532457d28edf29460a64b00f2a2
Viswa Chaitanya Sai Jada <viswachaitanya444@gmail.com>
2024-01-23 01:36:07 +0530

    created price component

---
+++ client/src/components/Prices.jsx
@@ -0,0 +1,27 @@
+export const Prices = [
+    {
+        _id:0,
+        name:"Rs. 0 - 500",
+        array:[0,500]
+    },
+    {
+        _id:1,
+        name:"Rs. 500 - 1000",
+        array:[500,1000]
+    },
+    {
+        _id:2,
+        name:"Rs. 1000 - 5000",
+        array:[1000,5000]
+    },
+    {
+        _id:3,
+        name:"Rs. 5000 - 10,000",
+        array:[5000,10000]
+    },
+    {
+        _id:4,
+        name:"Rs. 10,000 or more",
+        array:[10000,9999999]
+    },
+]
