69cc326f0b05b002aabd669d5e51c43bcaee5a3d
Viswa Chaitanya Sai Jada <viswachaitanya444@gmail.com>
2024-02-10 20:48:20 +0530

    initial deploy setup

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-import express  from "express";
-import colors  from "colors";
+import express from "express";
+import colors from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
@@ -7,6 +7,7 @@ import authRoutes from "./routes/authRoute.js"
 import cors from "cors"
 import categoryRoute from "./routes/categoryRoute.js"
 import productRoute from "./routes/productRoute.js"
+import path from "path"
 
 dotenv.config();
 
@@ -20,20 +21,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, './client/build')));
 
 //routes
-app.use('/api/v1/auth',authRoutes);
-app.use('/api/v1/category',categoryRoute);
+app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/category', categoryRoute);
 app.use('/api/v1/product', productRoute);
 
 //rest api
-app.get("/",(req,res)=>{
-    res.send("<h1>Welcome to my ecommerce website</h1>");
-} );
+app.use("*", function (req, res) {
+    res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
 
 //port
 const PORT = process.env.PORT;
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server is connected to ${PORT}`.bgCyan.white);
 })
