67d19e9ec475c7b1ccf8883d5c2217e2239a4bcc
Viswa Chaitanya Sai Jada <viswachaitanya444@gmail.com>
2024-02-10 20:53:24 +0530

    initial deploy setup

--- server.js
+++ server.js
@@ -8,11 +8,16 @@ import cors from "cors"
 import categoryRoute from "./routes/categoryRoute.js"
 import productRoute from "./routes/productRoute.js"
 import path from "path"
+import { fileURLToPath } from "url"
 
 dotenv.config();
 
 //database config
 connectDB();
+
+//esmodule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //rest object
 const app = express();
