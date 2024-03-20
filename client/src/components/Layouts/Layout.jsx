9df4e2a4165248108eb79b8d6c465fa21f6d23df
Viswa Chaitanya Sai Jada <viswachaitanya444@gmail.com>
2024-03-21 01:03:41 +0530

    frontend

--- client/src/components/Layouts/Layout.jsx
+++ client/src/components/Layouts/Layout.jsx
@@ -1,19 +1,14 @@
 import React from 'react'
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
+import Buffer from '../Buffer/Buffer';
 // import {Helmet} from "react-helmet";
 import { Toaster } from 'react-hot-toast';
 
 const Layout = ({children}) => {
   return (
     <>
-      {/* <Helmet>
-          <meta charSet="utf-8" />
-          <meta name="description" content={description}/>
-          <meta name="keywords" content={keywords}/>
-          <meta name="author" content={auther}/>
-          <title>{title}</title>
-      </Helmet> */}
+      <Buffer/>
       <Header/>
         <main style={{minHeight:"75vh"}}>
           <Toaster/>
